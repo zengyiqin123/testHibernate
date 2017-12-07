31e1b5e0fce8fc9d7e90e29e133c4f56196f3ab4
zengyiqin123 <1245657871@qq.com>
2017-12-07 17:10:32 +0800

    增加了people类

---
+++ src/main/java/com/zeng/entity/People.java
@@ -0,0 +1,14 @@
+package com.zeng.entity;
+
+public class People {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
