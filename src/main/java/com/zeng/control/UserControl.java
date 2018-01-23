dcac104cd0516966c6116d5e3311e6b795a3b71f
PC-2018010201\admin060816 <m13117914131@163.com>
2018-01-23 18:21:01 +0800

    新增了show5();

---
+++ src/main/java/com/zeng/control/UserControl.java
@@ -0,0 +1,58 @@
+package com.zeng.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+import com.zeng.entity.Users;
+
+@Controller
+@RequestMapping("uc")
+public class UserControl {
+	/*@RequestMapping(value="islogin",method=RequestMethod.POST)
+	public String islogin(String loginname,String loginpwd){
+		System.out.println("loginname:"+loginname+",loginpwd:"+loginpwd);;
+		return "user/suc";
+	}*/
+	/*@RequestMapping(value="islogin",method=RequestMethod.POST)
+	public String islogin(Users users){
+		System.out.println(users);
+		return "user/suc";
+	}*/
+	@RequestMapping(value="checklogin",method=RequestMethod.GET)
+	public String islogin(Model model){
+		model.addAttribute(new Users());
+		System.out.println("经过了islogin");
+		return "user/login2";
+	}
+	@RequestMapping(value="checklogin",method=RequestMethod.POST)
+	public String islogin(Users user){
+		System.out.println("经过了islogin");
+		System.out.println(user);
+		return "user/suc";
+	}
+	@RequestMapping(value="chainlogin",method=RequestMethod.GET)
+	public ModelAndView chainlogin(Users users){
+		ModelAndView mav = new ModelAndView();
+		System.out.println(users.getLoginname()+","+users.getLoginpwd());
+		users.setSex("man");
+		mav.addObject(users);
+		mav.setViewName("redirect:/uc/showinfo");
+		return mav;
+	}
+	
+	@RequestMapping(value="showinfo",method=RequestMethod.GET)
+	public String showinfo(Model model){
+		model.addAttribute("msg", "用户信息");
+		return "user/suc";
+	}
+	
+	@RequestMapping(value="showinfo2",method=RequestMethod.GET)
+	public String showinfo2(Model model){
+		model.addAttribute("msg", "用户信息");
+		return "user/suc";
+	}
+}
---
+++ src/main/webapp/user/login2.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Insert title here</title>
+</head>
+<body>
+<form:form modelAttribute="users">
+	<form:input path="loginname"/>
+	<form:password path="loginpwd"/>
+	<input type="submit" value="登录">
+</form:form>
+</body>
+</html>
